d35543648a6212b20b3dec2625ab03aee55a591f
= <chris_william_@outlook.com>
2024-08-03 12:22:06 +0100

    day7

---
+++ 11.map/108.Redundant-Links.cpp
@@ -0,0 +1,52 @@
+/*
+
+https://kamacoder.com/problempage.php?pid=1181
+2024-8-3
+
+*/
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int n; // 节点数量
+vector<int> father(1001, 0); // 按照节点大小范围定义数组
+
+// 并查集里寻根的过程
+int find(int u) {
+    if(u == father[u]){
+        return u;
+    }else{
+        return father[u] = find(father[u]);
+    }
+}
+// 判断 u 和 v是否找到同一个根
+bool isSame(int u, int v) {
+    u = find(u);
+    v = find(v);
+    return u == v;
+}
+// 将v->u 这条边加入并查集
+void join(int u, int v) {
+    u = find(u); // 寻找u的根
+    v = find(v); // 寻找v的根
+    if (u == v) return ; // 如果发现根相同，则说明在一个集合，不用两个节点相连直接返回
+    father[v] = u;
+}
+
+int main() {
+    int s, t;
+    cin >> n;
+    for (int i = 0; i <= n; ++i) {
+        father[i] = i;
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> s >> t;
+        if (isSame(s, t)) {
+            cout << s << " " << t << endl;
+            return 0;
+        } else {
+            join(s, t);
+        }
+    }
+}
